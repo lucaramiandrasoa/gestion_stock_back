e64a8b4c08b19fba1f172a3be95327428506a4ed
404 <fabrucemaminiaina@gmail.com>
2022-11-04 15:12:25 +0300

    recupération compte

--- controllers/authController.js
+++ controllers/authController.js
@@ -3,6 +3,45 @@ const { QueryTypes } = require('sequelize');
 const jwt = require('jsonwebtoken') //Token
 const bcrypt = require('bcrypt') //Cryptage
 
+module.exports.account_recovery = async(req,res) => {
+    try {
+        const {id_utilisateur, id_question_securite, reponse_securite} = req.body
+        const user = await Utilisateur.findOne({id: id_utilisateur})
+        user.id_question_securite === id_question_securite && user.reponse_securite === reponse_securite ? res.json(true) : res.json(false)
+    } catch (error) {
+        console.log(error)
+        res.json("error")
+    }
+}
+
+module.exports.change_password = async(req,res) => {
+    try {
+        const {id_utilisateur, new_password} = req.body
+        const user = await Utilisateur.findOne({id: id_utilisateur})
+        const salt = await bcrypt.genSalt()
+        user.password = await bcrypt.hash(new_password, salt) 
+        user.save()
+        res.json(true)
+    } catch (error) {
+        console.log(error)
+        res.json(false)
+    }
+}
+
+module.exports.signup = async(req,res) => {
+    try{
+        var {email, password, id_question_securite, reponse_securite} = req.body
+        const salt = await bcrypt.genSalt()
+        password = await bcrypt.hash(password, salt)
+        const utilisateur = await Utilisateur.create({email, password, id_question_securite, reponse_securite})
+        res.json(utilisateur)
+    }
+    catch(err){
+        console.log(err)
+        res.json(false)
+    }
+}
+
 module.exports.signin = async(req,res) => {
     try {
         var {email,password} = req.body
@@ -22,19 +61,6 @@ module.exports.signin = async(req,res) => {
         }
     } catch (error) {
         console.log(error)
-        res.json(false)
-    }
-}
-module.exports.signup = async(req,res) => {
-    try{
-        var {email, password, id_question_securite, reponse_securite} = req.body
-        const salt = await bcrypt.genSalt()
-        password = await bcrypt.hash(password, salt)
-        const utilisateur = await Utilisateur.create({email, password, id_question_securite, reponse_securite})
-        res.json(utilisateur)
-    }
-    catch(err){
-        console.log(err)
         res.json(false)
     }
 }
--- routes/routes.js
+++ routes/routes.js
@@ -2,6 +2,9 @@ const authController = require('../controllers/authController')
 const {Router} = require('express')
 const router = Router()
 
+
+router.post('/account_recovery', authController.account_recovery)
+router.post('/change_password', authController.change_password)
 router.post('/signin', authController.signin)
 router.post('/signup', authController.signup)
 router.get('/logout', authController.logout)
